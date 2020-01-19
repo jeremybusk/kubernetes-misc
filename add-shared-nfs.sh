7de3591bc541edac22d0d5294906884b4ddc8137
Jeremy Busk <jeremybusk@gmail.com>
2020-01-19 12:09:34 -0700

    Create add-shared-nfs.sh

---
+++ add-shared-nfs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+sudo apt install nfs-common
+sudo mkdir -p /data/nfs
+sudo chmod 0777 /data/nfs
+echo "nas1.example.com:/slow/kub-c1       /data/nfs      nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0" > /etc/fstab
+# # nas1.example.com:/home /data/nfs nfs rw,hard,intr,rsize=8192,wsize=8192,timeo=14 0 0
